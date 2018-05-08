89c5b6b477664842b7843afe216228e0a7944dd2
devyatka <strelmaria@mail.ru>
2018-05-08 23:22:57 +0500

    corrected generate

--- tasks-deploy/secure-library/generate.py
+++ tasks-deploy/secure-library/generate.py
@@ -1,5 +1,7 @@
 TITLE = "Защищенная библиотека"
-STATEMENT_TEMPLATE = "В этой библиотеке можно прочитать любую книгу, если захотеть."
+STATEMENT_TEMPLATE = '''В этой библиотеке можно прочитать любую книгу, если захотеть.
+
+[library](/static/files/<task.id>/library)'''
 
 def generate(context):
     return TaskStatement(TITLE, STATEMENT)
