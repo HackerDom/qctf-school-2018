a76b59a8b418e0a04956d36b73ff27683f0e0a0d
Vladimir <twnk@list.ru>
2018-05-13 03:39:10 +0500

    redockerized + links

--- tasks-deploy/secure-library/generate.py
+++ tasks-deploy/secure-library/generate.py
@@ -2,6 +2,8 @@ TITLE = "Защищенная библиотека"
 STATEMENT_TEMPLATE = '''
 В этой библиотеке можно прочитать любую книгу, если захотеть.
 
+`nc secure-library.contest.qctf.ru 20003`
+
 [library](/static/files/<task.id>/library)
 '''
 
