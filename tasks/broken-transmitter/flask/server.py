bb0f24b97302772ec8b4d45093c29e2a8d85c40c
Vladimir <twnk@list.ru>
2018-05-13 12:32:55 +0500

    bad transmitter

--- tasks/broken-transmitter/flask/server.py
+++ tasks/broken-transmitter/flask/server.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 
-import time
+from time import time
 
 from flask import Flask, request, render_template, redirect, url_for
 
 from tokens import flags
-from reader import Reader
 
 
 app = Flask(__name__)
-reader = Reader(
-    filename='text.txt', 
-    flag_length=len(list(flags.values())[0]), 
-    step=16, 
-    delay=3
-)
 
+TOKENS = dict()
+PART_LENGTH = 16
+DELAY = 5
+TEXT = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.   '''
+
+
+
+def tohex(text):
+    hexcode = lambda symbol: hex(ord(symbol))[2:].zfill(2)
+    ' '.join(map(hexcode, result))
+    
 
 @app.route('/')
 def wrong_token():
@@ -24,10 +28,20 @@ def wrong_token():
 
 @app.route('/<token>/')
 def show(token):
+    global TOKENS
+    flag = flags[token]
+    message = TEXT + flag
+
     if token not in flags:
         return redirect(url_for('wrong_token'))
-    flag = flags[token]
-    return render_template('show.html', message=reader.read(flag))
+    if token not in TOKENS:
+        TOKENS[token] = (time(), 0)
+    elif time() - TOKENS[token][0] > DELAY:
+        TOKENS[token] = (time(), TOKENS[token][1] + PART_LENGTH)
+    if TOKENS[token][1] + PART_LENGTH > len(message):
+        TOKENS[token] = (time(), 0)
+
+    return render_template('show.html', message=message[TOKENS[token][1]:TOKENS[token][1] + PART_LENGTH])
 
 
 if __name__ == '__main__':
