36f7c64f1fe77bc755e954edb0266eb235b1e8ae
1MiKHalyCH1 <bykoun@gmail.com>
2018-05-13 17:39:10 +0500

    maybe cheryl 500 fix

--- tasks/cheryl/flask/gunicorn.conf.py
+++ tasks/cheryl/flask/gunicorn.conf.py
@@ -3,7 +3,7 @@ import multiprocessing
 user      = 'www-data'
 group     = 'www-data'
 bind      = '0.0.0.0:80'
-workers   = multiprocessing.cpu_count() * 2 + 1
+workers   = 1
 threads   = workers
 accesslog = '-'
 errorlog = '-'
